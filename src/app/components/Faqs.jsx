98cb9905a3d2045ea6bf7aa251f40b9525e8d47f
Raghav-558 <jangraraghav9211@gmail.com>
2025-01-27 15:45:57 +0530

    add faq hover

--- src/app/components/Faqs.jsx
+++ src/app/components/Faqs.jsx
@@ -1,6 +1,7 @@
 'use client'
 import React, { useState } from 'react'
 import { ArrowIcon } from '../utils/icons'
+import CustomHeading from './common/CustomHeading'
 
 const Faqs = () => {
   const FAQ_LIST = [
@@ -58,19 +59,33 @@ const Faqs = () => {
     setActive(active === i ? null : i)
   }
 
+  const handleParentClick = (i) => {
+    toggle(i)
+  }
+
   return (
-    <div className='pt-[210px] max-lg:pb-[60px] max-lg:pt-[100px]' id='faq'>
-      <div className='max-w-[1174px] mx-auto px-4 mb-[-1px]'>
-        <div className='max-w-[754px] mx-auto flex flex-col gap-2'>
+    <div className='pt-[93px] pb-[137px] max-lg:py-20 max-md:py-[60px]'>
+      <div className='max-w-[1172px] mx-auto px-4'>
+        <CustomHeading
+          title='Frequently asked questions?'
+          classStyle='text-center font-kanit max-w-[734px] mx-auto leading-16xl'
+        />
+        <div className='max-w-[754px] mx-auto flex flex-col gap-2 pt-[46px] max-lg:pt-10 max-md:pt-7'>
           {FAQ_LIST.map((obj, i) => (
             <div
               key={i}
-              className='rounded-2xl border-2 border-solid border-black border-opacity-70 pt-[17px] pr-[18px] pb-[15px] pl-5 max-md:p-4 max-sm:p-3'
+              className='rounded-2xl border-2  border-solid border-black border-opacity-70'
+              onClick={() => handleParentClick(i)}
             >
               <button
-                onClick={() => toggle(i)}
-                className={`flex items-center justify-between font-medium text-xl text-black w-full leading-custom-9xl text-left max-md:text-lg max-md:leading-7 max-sm:text-base max-sm:leading-6 ${
-                  active === i ? 'pb-[11px]' : ''
+                onClick={e => {
+                  e.stopPropagation()
+                  toggle(i)
+                }}
+                className={`flex items-center justify-between font-kanit font-medium text-xl text-black w-full leading-custom-9xl text-left max-md:text-lg max-md:leading-7 max-sm:text-base max-sm:leading-6 pt-[17px] pr-[18px] pl-5 !pb-[15px] max-sm:p-4 max-sm:!pb-4 ${
+                  active === i
+                    ? '!pb-[11px] max-sm:!pb-[11px]'
+                    : 'pb-0 transition-all duration-300'
                 }`}
               >
                 {obj.title}
@@ -83,14 +98,16 @@ const Faqs = () => {
                 </span>
               </button>
               <div
-                className={`max-w-[716px] text-black transition-all duration-300 overflow-hidden max-sm:overflow-y-auto ${
-                  active === i ? 'pb-[5px] max-h-40' : 'max-h-0'
+                className={`text-black transition-all duration-300 overflow-hidden pl-5 pr-[18px] max-sm:px-4 ${
+                  active === i ? 'pb-[20px] max-sm:pb-4 max-h-40' : 'max-h-0'
                 }`}
               >
-                <p className='text-black leading-6 !max-md:text-sm max-md:leading-6 max-sm:text-sm'>{obj.description}</p>
-                <p className='font-semibold leading-6 text-dark-green !pt-[9px] max-md:!pt-1 max-md:text-sm max-md:leading-6 max-sm:text-sm'>
-                  {obj.secondDescription}
+                <p className='text-black leading-6 !max-md:text-sm max-md:leading-6 max-sm:text-sm pb-[9px] max-md:pb-1 max-w-[716px]'>
+                  {obj.description}
                 </p>
+                <a href='/' className='font-semibold leading-6 text-dark-green max-md:text-sm max-md:leading-6 max-sm:text-sm hover:text-blue-400 transition-all duration-300'>
+                  {obj.secondDescription}
+                </a>
               </div>
             </div>
           ))}
--- src/app/components/RevenueChannel.jsx
+++ src/app/components/RevenueChannel.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import CustomButton from './common/CustomButton'
+import CustomHeading from './common/CustomHeading'
 
 const RevenueChannel = () => {
   return (
-    <div>RevenueChannel</div>
+    <div className='bg-light-green rounded-[40px] pt-[90px] pb-[103px]'>
+      <div className='max-w-[1172px] px-4 mx-auto'>
+        <div className='flex items-center justify-between'>
+          <div>
+            <p className='font-kanit text-xl font-semibold leading-5 text-dark-green'>
+              Let's take about sales
+            </p>
+            <CustomHeading title="Turn fulfillment into your #1 revenue channel" classStyle=""/>
+            <p className='text-black opacity-80 max-w-[510px] leading-6 pt-[11px]'>
+              We helped thousands of merchants scale their business to new
+              heights. AllSome customers achieve 340% growth overtime with auto
+              run e-commerce post purchase operations.
+            </p>
+            <CustomButton title="Chat with our fulfillment experts" classStyle='mt-[23px] !py-3' />
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
