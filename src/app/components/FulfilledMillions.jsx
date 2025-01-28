284c77474a1b6f7d1656adb52fb66f9e385dc362
ritu-477 <ritujoharbishnoi@gmail.com>
2025-01-28 12:48:37 +0530

    done

--- src/app/components/FulfilledMillions.jsx
+++ src/app/components/FulfilledMillions.jsx
@@ -1,45 +1,10 @@
 import React from 'react'
 import CustomSlider from './common/CustomSlider'
 import CustomHeading from './common/CustomHeading'
+import { OVERFLOW_SWIPER_DATA_LIST,OPPOSITESIDE_ONE_DATA_LIST,TRUST_LIST } from '../utils/helper'
 
 const FulfilledMillions = () => {
-  const OVERFLOW_SWIPER_DATA_LIST = [
-    { image: "/assets/images/webp/soft-serve.webp", width: 90.67, height: 32 },
-    { image: "/assets/images/webp/brightland.webp", width: 45, height: 32 },
-    { image: "/assets/images/webp/kopari.webp", width: 90.92, height: 32 },
-    { image: "/assets/images/webp/crossnet.webp", width: 91.92, height: 32 },
-    { image: "/assets/images/webp/fly-by-jing.webp", width: 90.92, height: 32 },
-    { image: "/assets/images/webp/squatch.webp", width: 91.92, height: 32 },
-    { image: "/assets/images/webp/death-wish.webp", width: 87, height: 32 },
-    { image: "/assets/images/webp/native.webp", width: 91.59, height: 32 },
-
-  ]
-
-  const OPPOSITESIDE_ONE_DATA_LIST = [
-    { image: "/assets/images/webp/native.webp", width: 91.59, height: 32 },
-    { image: "/assets/images/webp/soft-serve.webp", width: 90.67, height: 8},
-    { image: "/assets/images/webp/brightland.webp", width: 45, height: 33 },
-    { image: "/assets/images/webp/kopari.webp", width: 90.92, height: 33 },
-    { image: "/assets/images/webp/crossnet.webp", width: 91.92, height: 33 },
-    { image: "/assets/images/webp/fly-by-jing.webp", width: 90.92, height: 33 },
-    { image: "/assets/images/webp/squatch.webp", width: 91.92, height: 23 },
-    { image: "/assets/images/webp/death-wish.webp", width: 87, height: 29 },
-    { image: "/assets/images/webp/native.webp", width: 91.59, height: 33 },
-
-
-  ]
-  const TRUST_LIST = [
-    { image: "/assets/images/webp/soft-serve.webp", width: 90.67, height: 32 },
-    { image: "/assets/images/webp/brightland.webp", width: 45, height: 32 },
-    { image: "/assets/images/webp/kopari.webp", width: 90.92, height: 32 },
-    { image: "/assets/images/webp/crossnet.webp", width: 91.92, height: 32 },
-    { image: "/assets/images/webp/fly-by-jing.webp", width: 90.92, height: 32 },
-    { image: "/assets/images/webp/squatch.webp", width: 91.92, height: 32 },
-    { image: "/assets/images/webp/death-wish.webp", width: 87, height: 32 },
-    { image: "/assets/images/webp/native.webp", width: 91.59, height: 32 },
-
-  ]
-
+ 
   return (
     <div className='pt-[89px] pb-[76px] max-w-[1920px] mx-auto relative'>
       <div></div>
