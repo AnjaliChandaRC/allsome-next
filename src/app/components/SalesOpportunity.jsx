632eb3464be86e68e48b412be7a70a7a7f13a822
Seema-476 <simmybishnoi29085@gmail.com>
2025-01-28 15:39:03 +0530

    changes

--- src/app/components/SalesOpportunity.jsx
+++ src/app/components/SalesOpportunity.jsx
@@ -13,7 +13,7 @@ const SalesOpportunity = () => {
             <div className='xl:max-w-[560px] w-full flex flex-col lg:items-start items-center justify-center xl:pt-10'>
               <CustomHeading styleclass="xl:!text-custom-5xl xl:!leading-custom-15xl lg:text-5xl md:text-4xl text-2xl font-kanit font-semibold max-xl:text-center sm:pb-[21px] pb-4"
                 title={'Maximize sales opportunity with ready inventory'} />
-              <CustomDescription styleclass="xl:max-w-[436px] !text-black w-full sm:!text-base sm:!leading-6 text-sm opacity-80 font-normal max-lg:text-center"
+              <CustomDescription styleclass="xl:max-w-[436px] !text-black w-full sm:!text-base sm:!leading-6 text-sm opacity-80 font-normal max-xl:text-center"
                 description="Be where your shoppers are, both online and offline. Appear to the right audience at the right time, both online and offline to generate sales for you." />
               <CustomButton styleclass="sm:mt-[25px] mt-4 !py-[10px] !px-[22px] !bg-black !text-white max-xl:mx-auto" title="Sign up" />
             </div>
