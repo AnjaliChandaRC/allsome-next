5fd523fa639c4f1186c5f7833c47dee72b964388
Raghav-558 <jangraraghav9211@gmail.com>
2025-01-28 12:45:46 +0530

    add earth image

--- src/app/components/RevenueChannel.jsx
+++ src/app/components/RevenueChannel.jsx
@@ -6,16 +6,19 @@ import CustomDescription from './common/CustomDescription'
 
 const RevenueChannel = () => {
   return (
-    <div className='bg-light-green rounded-[40px] pt-[90px] pb-[103px] max-xl:py-20 max-lg:py-[60px]'>
+    <div className='bg-light-green rounded-[40px] pt-[90px] pb-[103px] max-xl:py-20 max-lg:py-[60px] max-md:py-10 overflow-hidden'>
       <div className='container'>
-        <div className='flex items-center justify-between max-lg:flex-wrap max-lg:justify-center'>
+        <div className='flex items-center justify-between xl:gap-10 max-lg:flex-wrap max-lg:justify-center'>
           <div className='flex flex-col'>
             <p className='font-kanit text-xl font-semibold leading-5 text-dark-green max-lg:text-center pb-[11px]'>
               Let's take about sales
             </p>
             <CustomHeading
-              title='Turn fulfillment into your #1 revenue channel'
-              styleclass='max-w-[692px] font-kanit  max-lg:!max-w-none max-lg:text-center'
+              title='Turn fulfillment into your'
+              textTwo='#1 revenue'
+              titleTwo='channel'
+              styleclassTwo=''
+              styleclass='xl:max-w-[692px] font-kanit max-lg:!max-w-none max-lg:text-center'
             />
             <CustomDescription
               styleclass='!text-black !opacity-80 !max-w-[510px] !leading-6 !pt-[11px] sm:text-base max-lg:!max-w-none max-lg:text-center'
@@ -26,7 +29,7 @@ const RevenueChannel = () => {
             <div className='max-lg:flex max-lg:justify-center'>
               <CustomButton
                 title='Chat with our fulfillment experts'
-                styleclass='!mt-[23px] !py-3 max-lg:!mt-5'
+                styleclass='!mt-[23px] !py-[10.5px] !px-6 max-lg:!mt-5'
               />
             </div>
           </div>
@@ -35,7 +38,7 @@ const RevenueChannel = () => {
             alt='revenue-chart'
             width={483}
             height={357}
-            className='object-cover max-w-[483px] lg:h-[357px] w-full max-lg:pt-5'
+            className='object-cover pointer-events-none rounded-2xl max-w-[483px] lg:h-[357px] max-lg:max-w-[380px] max-md:max-w-[300px] w-full max-lg:pt-8 revenue-img-shadow'
           />
         </div>
       </div>
