c2451f0b337d8d3420089f4352fd9c6c8c5e38dd
anjali <anjalichanda2019@gmail.com>
2025-01-29 10:20:15 +0530

    resolve border issue in heading

--- src/app/components/common/CustomHeading.jsx
+++ src/app/components/common/CustomHeading.jsx
@@ -1,14 +1,18 @@
-import React from 'react'
+import React from 'react';
 
 const CustomHeading = (props) => {
   return (
     <h2 className={`${props.styleclass} xl:text-7xl font-kanit lg:text-6xl md:text-5xl sm:text-4xl text-3xl !leading-[100%] text-black font-semibold`}>
-      <span className={`${props.styleclassOne}text-dark-green border-b-[3px] border-b-dark-green rounded-[3px] !leading-[100%]`}> {props.text} </span>
+      <span className={`${props.styleclassOne} relative text-dark-green after:content-[''] after:block after:w-full after:h-[3px] after:bg-dark-green after:absolute after:bottom-[15px] after:left-0 after:rounded-[3px]`}>
+        {props.text}
+      </span>
       {props.title}
-      <span className={`${props.styleclassTwo}text-dark-green border-b-[3px] border-b-dark-green rounded-[3px] !leading-[100%]`}> {props.textTwo} </span>
+      <span className={`${props.styleclassTwo} relative text-dark-green after:content-[''] after:block after:w-full after:h-[3px] after:bg-dark-green after:absolute after:bottom-[15px] after:left-0 after:rounded-[3px]`}>
+        {props.textTwo}
+      </span>
       {props.titleTwo}
     </h2>
-  )
-}
+  );
+};
 
-export default CustomHeading
+export default CustomHeading;
