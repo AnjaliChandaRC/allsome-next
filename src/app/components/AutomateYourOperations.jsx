4462043b5b836ca95436a1ee868d46c25fedc687
ritu-477 <ritujoharbishnoi@gmail.com>
2025-01-29 10:15:03 +0530

    merge-section

--- src/app/components/AutomateYourOperations.jsx
+++ src/app/components/AutomateYourOperations.jsx
@@ -7,8 +7,8 @@ import CustomButton from './common/CustomButton'
 const AutomateYourOperations = () => {
   return (
     <div className='bg-light-green rounded-[40px]'>
-      <div className='container max-w-[1109px] mx-auto'>
-        <div className='lg:flex-row max-lg:flex-col-reverse flex lg:py-[112px] md:py-14 sm:py-10 py-8 justify-between flex-col gap-7'>
+      <div className='container lg:max-w-[1109px] mx-auto'>
+        <div className='lg:flex-row flex lg:py-[112px] sm:py-12 py-8 justify-between flex-col gap-7'>
           <div className='flex lg:justify-start justify-center'>
             <Image
               src="/assets/images/webp/automate-image.webp"
@@ -19,9 +19,9 @@ const AutomateYourOperations = () => {
             />
           </div>
           <div className='lg:w-[483px] w-full flex flex-col lg:items-start items-center justify-center'>
-            <CustomHeading styleclass="lg:max-w-[482px] xl:!text-custom-5xl xl:!leading-custom-15xl lg:text-5xl md:text-4xl text-2xl font-kanit font-semibold sm:pb-7 pb-4 max-lg:text-center" title={'Automate your operations'} />
+            <CustomHeading styleclass="lg:max-w-[482px] xl:!text-custom-5xl lg:text-5xl md:text-4xl text-2xl font-kanit font-semibold sm:pb-7 pb-4 max-lg:text-center" title={'Automate your operations'} />
             <CustomDescription styleclass="lg:max-w-[463px] !text-black w-full sm:!text-base sm:!leading-6 text-sm opacity-80 font-normal max-lg:text-center" description="Build your e-commerce operation teams with ready software, modules, workforce, and more - all within AllSome. Once sold, orders will be shipped out automatically." />
-            <CustomButton styleclass="sm:mt-[27px] mt-4 !py-[10px] !px-[22px] !bg-black !text-white" title="Sign up" />
+            <CustomButton styleclass="sm:mt-[27px] mt-4 !py-[10px] !px-[22px] !bg-black !text-white !w-[107px] !h-12 !hover:w-full !hover:text-black" title="Sign up" />
           </div>
 
         </div>
--- src/app/components/FulfilledMillions.jsx
+++ src/app/components/FulfilledMillions.jsx
@@ -12,14 +12,14 @@ const FulfilledMillions = () => {
                   src="/assets/images/webp/left-ellipse.webp"
                   width={471}
                   height={241}
-                  className='object-cover lg:block hidden sm:max-w-[300px] max-sm:h-[200px] max-w-[100px] lg:max-w-[471px] w-full absolute left-0 lg:top-[43%] z-10'
+                  className='object-cover lg:block hidden sm:max-w-[300px] max-sm:h-[200px] max-w-[100px] lg:max-w-[471px] w-full absolute left-0 lg:top-[43.2%] z-10'
                   alt="ellipse-left"
       />
       <Image
         src="/assets/images/webp/right-ellipse.webp"
         width={471}
         height={241}
-        className='object-cover lg:block hidden sm:max-w-[300px] max-w-[100px] max-sm:h-[200px] lg:max-w-[471px] w-full absolute right-0 lg:top-[43%] z-10'
+        className='object-cover lg:block hidden sm:max-w-[300px] max-w-[100px] max-sm:h-[200px] lg:max-w-[471px] w-full absolute right-0 lg:top-[43.2%] z-10'
         alt="ellipse-right"
       />
       <CustomHeading styleclass="max-w-[746px] mx-auto text-center font-kanit" title="We have fulfilled " textTwo="millions" styleclassTwo="" />
@@ -27,7 +27,6 @@ const FulfilledMillions = () => {
         <div className='py-[7px]'>
           <CustomSlider
             dataList={OVERFLOW_SWIPER_DATA_LIST}
-            classStyle=''
           />
        </div>
         <div className='py-[7px] lg:mt-[18px] mt-4'>
