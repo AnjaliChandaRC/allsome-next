9e022af83c92e5a4b2810aadb9066f9c84ada392
Seema-476 <simmybishnoi29085@gmail.com>
2025-01-29 16:37:12 +0530

    changes

--- src/app/components/DoMoreBest.jsx
+++ src/app/components/DoMoreBest.jsx
@@ -2,6 +2,7 @@ import Image from 'next/image';
 import CustomHeading from '../components/common/CustomHeading'
 import CustomDescription from './common/CustomDescription';
 import { DO_MORE_BEST_DATA } from '../utils/helper';
+import 'aos/dist/aos.css';
 
 const DoMoreBest = () => {
  
