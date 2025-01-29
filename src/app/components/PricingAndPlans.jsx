9b9e90a78459e2ad4bb92cf7121eac59791929f9
Ankush307 <ankushdhull966@gmail.com>
2025-01-29 15:32:20 +0530

    change top bottom spacing

--- src/app/components/PricingAndPlans.jsx
+++ src/app/components/PricingAndPlans.jsx
@@ -9,14 +9,14 @@ import { PRICING_DATA_LIST } from '../utils/helper'
 const PricingAndPlans = () => {
 
   return (
-    <div id='pricing' className="container sm:pb-[66px] max-sm:py-10 max-xl:pt-20 max-lg:pt-16">
-      <CustomHeading styleclass="lg:!text-7xl md:!text-6xl sm:!text-5xl !text-4xl pb-3.5 font-kanit font-bold text-center" title="Pricing & Plans" />
+    <div id='pricing' className="container sm:pb-10 md:pb-[66px] lg:pt-20 max-sm:py-[30px] max-xl:pt-20 max-lg:pt-[60px] max-md:pt-10 max-sm:pt-[30px]">
+      <CustomHeading styleclass="lg:!text-7xl md:!text-6xl sm:!text-5xl !text-4xl pb-3.5 font-kanit font-bold text-center !font-bold" title="Pricing & Plans" />
       <CustomDescription styleclass="text-center sm:pb-9 !pb-5" description="Free access to outlets, pay-as-you-go." />
       <div className="flex xl:gap-[39px] gap-4 max-lg:flex-wrap justify-center">
         {PRICING_DATA_LIST.map((card, index) => (
-          <div key={index} className={`w-full relative max-w-[303px] rounded-2xl sm:py-[31px] py-5 bg-cover bg-center plans-shadow transition-all duration-300 ease-linear bg-no-repeat ${index === 1 ? "bg-pricing-table" : " border-2 border-black/20 hover:border-transparent "} `}>
+          <div key={index} className={`w-full relative max-w-[303px] rounded-2xl sm:py-[31px] py-5 bg-cover bg-center hover:shadow-[0px_1px_5px_5px_#36eb58] transition-all duration-300 ease-linear bg-no-repeat ${index === 1 ? "bg-pricing-table" : " border-2 border-black/20 hover:border-transparent "} `}>
             <div className="sm:px-7 px-5">
-              <h3 className="text-[13px] font-semibold uppercase pb-[3px] ">{card.title}</h3>
+              <h3 className="text-sm font-semibold uppercase pb-[3px] ">{card.title}</h3>
               <p className="text-sm mb-4 opacity-80">{card.description}</p>
             </div>
             <div className={`sm:px-7 px-5 bg-ghost-white ${index === 1 ? "bg-white/20" : ""}`}>
@@ -27,10 +27,10 @@ const PricingAndPlans = () => {
               <ul className="space-y-[7px] pt-3 sm:pb-[35px] pb-4 text-left">
                 {card.features.map((feature, i) => (
                   <li key={i} className={`flex gap-3`}>
-                    <span className={`pt-1.5 ${index === 1 ? "icon" : ""}`}>
+                    <span className={`pt-1.5 ${index === 1 ? "check-icon" : ""}`}>
                       <CheckIcon />
                     </span>
-                    <span className="opacity-80 text-[13px]">{feature}</span>
+                    <span className="opacity-80 text-sm">{feature}</span>
                   </li>
                 ))}
               </ul>
