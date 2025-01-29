2f8518dbcdfe54411c98587db26c1b80043dfe75
Ankush307 <ankushdhull966@gmail.com>
2025-01-29 09:20:00 +0530

    spacing update

--- src/app/components/ContactUs.jsx
+++ src/app/components/ContactUs.jsx
@@ -78,30 +78,30 @@ const ContactUs = () => {
     <div className="overflow-hidden">
       <div className='container xl:pt-[67px]'>
         <CustomHeading styleclass="text-center font-bold font-kanit max-w-[983px] mx-auto md:pb-[85px] sm:pb-16 pb-10 " title="Can't make up your mind?" textTwo="Contact us" titleTwo=" today!" styleclassTwo="" />
-        <div className="flex xl:pb-[300px] sm:pb-[100px] pb-16 justify-center xl:justify-between max-xl:flex-wrap space-y-12 ">
+        <div className="flex xl:pb-[300px] md:pb-[100px] sm:pb-16 pb-10 justify-center max-xl:flex-col-reverse max-xl:flex-wrap xl:gap-16 ">
           <div className="gap-[11px] flex flex-col w-full xl:w-5/12 justify-center items-center">
             {CONTACT_DATA_LIST.map((contact, index) => (
               <div key={index} className={`flex rounded-2xl px-[18px] plans-shadow transition-all duration-300 ease-linear py-[23px] w-full max-w-[768px] xl:max-w-[432px] bg-gradient-to-r from-minty-green via-vivid-green to-vivid-green ${index === 2 ? "items-start" : "items-center"} `}>
-                <div className={`flex items-center justify-center max-sm:p-4 sm:size-[83.24px] rounded-full mr-4 bg-deep-green ${index === 2 ? "sm:p-6" : ""}`}>
+                <div className={`flex items-center justify-center max-sm:p-4 sm:size-[83.24px] max-sm:size-[67px] rounded-full mr-4 bg-deep-green ${index === 2 ? "sm:p-6" : ""}`}>
                   {contact.icon}
                 </div>
                 <div>
                   <h2 className="text-sm font-medium leading-custom-xl pb-2.5">{contact.title}</h2>
-                  <a href={contact.link} target='_blank' className={`md:text-2xl font-kanit sm:text-xl text-lg leading-custom-4xl font-semibold ${index === 2 ? "sm:leading-custom-5xl max-sm:!leading-custom-3xl sm:!text-xl max-sm:text-base" : ""}`}>{contact.description}</a>
+                  <a href={contact.link} className={`md:text-2xl font-kanit sm:text-xl text-lg leading-custom-4xl font-semibold ${index === 2 ? "sm:leading-custom-5xl max-sm:!leading-custom-3xl sm:!text-xl max-sm:text-base" : ""}`}>{contact.description}</a>
                 </div>
               </div>
             ))}
           </div>
-          <div className="relative w-full xl:w-6/12">
-            <Image className='z-[-1] translate-y-[55px] absolute max-xl:hidden xl:right-[-35%] rounded-l-[40px] max-h-[593px] h-full' width={740} height={593} src="/assets/images/webp/contact-green-box.webp" alt='contact green box' />
+          <div className="relative w-full xl:w-6/12 max-xl:mb-12 max-sm:mb-8">
+            <Image className='z-[-1] translate-y-[55px] absolute max-xl:hidden xl:right-[-35%] rounded-l-[40px] h-[593px] max-w-[740px] w-full' width={740} height={593} src="/assets/images/webp/contact-green-box.webp" alt='contact green box' />
             <form ref={form} onSubmit={submitHandle}>
-              <div className="bg-gradient-to-r from-minty-green via-vivid-green to-vivid-green xl:!bg-white xl:p-0 md:p-8 sm:p-7 p-3 rounded-2xl sm:rounded-[40px] max-w-[586px] xl:max-w-[506px] ">
+              <div className="bg-gradient-to-r from-minty-green via-vivid-green to-vivid-green xl:!bg-white xl:p-0 md:p-8 sm:p-7 p-3 rounded-2xl sm:rounded-[40px] max-w-[586px] xl:max-w-[506px] max-xl:mx-auto">
                 <div className="flex flex-col w-full bg-white xl:max-w-[506px] form-box-shadow rounded-2xl sm:pt-[42px] sm:pb-[38px] sm:px-[29px] px-3 py-8">
                   <p className='md:text-custom-2xl sm:text-2xl text-xl font-semibold font-kanit leading-8 sm:pb-[13px] '>Get in touch!</p>
                   <CustomInput label="Your name*" value={value.name} onChange={(e) => setValue({ ...value, name: e.target.value })} type="name" placeholder="Jhone Doe" errorMessage={error && !value.name ? "Name is required" : ""} />
                   <CustomInput label="Your email*" value={value.email} onChange={(e) => setValue({ ...value, email: e.target.value })} type="email" placeholder="jhonedoe32@gmail.com" errorMessage={error && !value.email ? "Email is required" : ""} />
                   <CustomInput label="Your mobile" value={value.number} onChange={(e) => setValue({ ...value, number: e.target.value })} type="number" placeholder="+1   123 123 1234" errorMessage={error && !value.number ? "Number is required" : ""} />
-                  <CustomButton styleclass="!bg-black !text-white max-w-[153px] sm:mt-[33px] mt-5 !py-3" title="Next" />
+                  <CustomButton styleclass="!bg-black !text-white max-w-[153px] sm:mt-[31px] mt-5 !py-3 hover:!text-black" title="Next" />
                 </div>
               </div>
             </form>
