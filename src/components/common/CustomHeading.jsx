f37519244be4e62e49ad8f87dc2a3b87508660ec
ritu-477 <ritujoharbishnoi@gmail.com>
2025-01-31 14:17:37 +0530

    changes-done

--- src/components/common/CustomHeading.jsx
+++ src/components/common/CustomHeading.jsx
@@ -1,11 +1,11 @@
 const CustomHeading = ({ styleclass, styleclassOne, text, title, styleclassTwo, textTwo, titleTwo }) => {
   return (
     <h2 className={`${styleclass} xl:text-7xl !font-kanit lg:text-6xl md:text-5xl sm:text-4xl text-3xl lg:!leading-[100%] !leading-[120%] text-black !font-semibold`}>
-      <span className={`${styleclassOne} !font-kanit relative text-dark-green after:content-[''] after:block after:w-full after:h-[3px] after:bg-dark-green after:absolute after:xl:bottom-2 after:lg:bottom-1.5 after:md:bottom-1 after:bottom-0.5 after:left-0 after:rounded-[3px]`}>
+      <span className={`${styleclassOne} !font-kanit relative text-dark-green after:content-[''] after:block after:w-full after:h-[3px] after:bg-dark-green after:absolute after:xl:bottom-2.5 after:lg:bottom-1.5 after:md:bottom-1 after:bottom-0.5 after:left-0 after:rounded-[3px]`}>
         {text}
       </span>
       {title}
-      <span className={`${styleclassTwo} !font-kanit relative text-dark-green after:content-[''] after:block after:w-full after:h-[3px] after:bg-dark-green after:absolute after:xl:bottom-2 after:lg:bottom-1.5 after:md:bottom-1 after:bottom-0.5 after:left-0 after:rounded-[3px]`}>
+      <span className={`${styleclassTwo} !font-kanit relative text-dark-green after:content-[''] after:block after:w-full after:h-[3px] after:bg-dark-green after:absolute after:xl:bottom-2.5 after:lg:bottom-1.5 after:md:bottom-1 after:bottom-0.5 after:left-0 after:rounded-[3px]`}>
         {textTwo}
       </span>
       {titleTwo}
