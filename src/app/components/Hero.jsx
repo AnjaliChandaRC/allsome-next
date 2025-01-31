62d3a630ad2b88befe11bd90cc94aa57148e67c6
AmitSheoran-557 <amit22082006@gmail.com>
2025-01-31 09:26:43 +0530

    png to webp img

--- src/app/components/Hero.jsx
+++ src/app/components/Hero.jsx
@@ -31,7 +31,7 @@ const Hero = () => {
               <Image data-aos="fade-up" data-aos-delay="500" data-aos-duration="1000" className='shadow-[25px_-20px_60px_0px_#0000001A] pointer-events-none xl:max-w-[318] lg:block max-lg:hidden rounded-[29px] lg:max-w-64 lg:absolute xl:!translate-x-8 lg:!-translate-x-10 xl:-bottom-16 -bottom-10' src='/assets/images/webp/hero-mobile-img.webp' width={318} height={430} alt='mobile-img' />
               <div className="max-lg:relative justify-center flex">
                 <Image className='max-lg:block pointer-events-none lg:hidden relative w-full max-w-2xl' src='/assets/images/webp/hero-mobile-location-img.webp' width={635} height={543} alt='location-img' />
-                <Image className='max-lg:block pointer-events-none lg:hidden w-full z-50 md:max-w-44 sm:max-w-32 max-w-28 absolute md:top-14 sm:top-12 top-10 md:left-0 sm:left-7 left-3' src='/assets/images/webp/mobile-screen-img-2.png' width={635} height={543} alt='mobile-img' />
+                <Image className='max-lg:block pointer-events-none lg:hidden w-full z-50 md:max-w-44 sm:max-w-32 max-w-28 absolute md:top-14 sm:top-12 top-10 md:left-0 sm:left-7 left-3' src='/assets/images/webp/mobile-screen-img-2.webp' width={635} height={543} alt='mobile-img' />
               </div>
             </div>
           </div>
