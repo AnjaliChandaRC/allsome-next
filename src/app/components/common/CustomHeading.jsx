907d83fe9ae0271aa57fc0eaa9998f1a3f5f16d5
ritu-477 <ritujoharbishnoi@gmail.com>
2025-01-31 09:59:24 +0530

    done

--- src/app/components/common/CustomHeading.jsx
+++ src/app/components/common/CustomHeading.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const CustomHeading = (styleclass, styleclassOne, text, title, styleclassTwo, textTwo, titleTwo) => {
+const CustomHeading = ({ styleclass, styleclassOne, text, title, styleclassTwo, textTwo, titleTwo }) => {
   return (
     <h2 className={`${styleclass} xl:text-7xl font-kanit lg:text-6xl md:text-5xl sm:text-4xl text-3xl lg:!leading-[100%] !leading-[120%] text-black font-semibold`}>
       <span className={`${styleclassOne} relative text-dark-green after:content-[''] after:block after:w-full after:h-[3px] after:bg-dark-green after:absolute after:xl:bottom-2 after:lg:bottom-1.5 after:md:bottom-1 after:bottom-0.5 after:left-0 after:rounded-[3px]`}>
